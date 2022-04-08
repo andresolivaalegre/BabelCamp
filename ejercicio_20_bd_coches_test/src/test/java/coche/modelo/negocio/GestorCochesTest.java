c68a3979494b1dd5ca97f207a400cb41cba25f3c
Andres Oliva <andres.oliva@1AOLIVAXL5520.babel-si.local>
2022-04-08 13:36:26 +0200

    ejercicio 20 parte 2 (pendiente parte 3)

---
+++ ejercicio_20_bd_coches_test/src/test/java/coche/modelo/negocio/GestorCochesTest.java
@@ -0,0 +1,56 @@
+package coche.modelo.negocio;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import entidad.Coche;
+import main.Main;
+
+
+public class GestorCochesTest {
+	
+	@Test
+	public void GetPorIdTest() {
+		
+		//Meto id valido y existe
+		int id=2;
+		GestorCochesTest gc = new GestorCochesTest();
+		//Esto es una superchapuza, pero implementé los metodos en el main:( Felix porfa no me mates:( :( :(
+		Coche cocheEsperado = new Coche();
+		cocheEsperado.setId(2);
+		cocheEsperado.setKm(324324);
+		cocheEsperado.setMarca("Seat");
+		cocheEsperado.setMatricula("4736FTY");
+		cocheEsperado.setModelo("Cordoba");
+		assertEquals(cocheEsperado, Main.buscarId(id));
+		
+		//Meto id negativo
+		id=-15;
+		gc = new GestorCochesTest();
+		cocheEsperado = new Coche();
+		cocheEsperado.setId(0);
+		cocheEsperado.setKm(0);
+		cocheEsperado.setMarca("");
+		cocheEsperado.setMatricula("");
+		cocheEsperado.setModelo("");
+		assertEquals(cocheEsperado, Main.buscarId(id));
+		
+		//Meto id valido y no existe
+		
+		id=10;
+		gc = new GestorCochesTest();
+		cocheEsperado = new Coche();
+		cocheEsperado.setId(0);
+		cocheEsperado.setKm(0);
+		cocheEsperado.setMarca("");
+		cocheEsperado.setMatricula("");
+		cocheEsperado.setModelo("");
+		assertEquals(cocheEsperado, Main.buscarId(id));
+		
+		
+	}
+
+}
